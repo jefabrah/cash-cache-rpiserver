7f2d4e72fed20dc8cb7d4e2e8dec7550a66943d4
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-09-27 21:01:16 -0400

    counts button presses in timeout period

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 // button is attaced to pin 17, led to 18
 var GPIO = require('onoff').Gpio,
     led = new GPIO(18, 'out'),
-    button = new GPIO(17, 'in', 'rising');
+    button = new GPIO(17, 'in', 'rising'),
+    buttonCount = 0,
+    pulsing = false;
 
-// define the callback function
-function light(err, state) {
-  
-  if (err) console.log('err: ', err);
-  console.log('state: ', state);
-  // check the state of the button
-  // 1 == pressed, 0 == not pressed
-  if(state == 1) {
-    // turn LED on
-    led.writeSync(1);
-   setTimeout(function () {
-    console.log('LED off');
-    led.writeSync(0);
-   }, 100)
+// callback function for button press
+function handlePulse (err, state) {
+
+  if (err) {
+    console.log('err: ', err);
+    return;
   }
-
+  console.log('state: ', state);
+  // if button state does not equal 1 log
+  // & return out of function
+  if(state !== 1) {
+    console.log('State did not equal 1!?');
+    return;
+  }
+  // increment pulse count
+  buttonCount++;
+  // if pulsing started return
+  if (pulsing) return;
+  pulsing = true;
+  // otherwise turn on led &
+  // set timeout to clear count & turn off led
+  led.writeSync(1);
+  setTimeout(function () {
+    led.writeSync(0);
+    console.log('LED off');
+    console.log('Button count:', buttonCount);
+    buttonCount = 0;
+    console.log('Button count reset to:', buttonCount);
+    pulsing = false;
+  }, 6000)
 }
 
-// pass the callback function to the
-// as the first argument to watch()
-button.watch(light);
+// pass the button callback function to the 
+button.watch(handlePulse);
