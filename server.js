c60f4fb301fb91bbfb1637d84fa1ffcb38321458
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-09-27 20:02:59 -0400

    only detect rising state on button input, log button state & error on change, set timeout to turn off LED output

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // button is attaced to pin 17, led to 18
 var GPIO = require('onoff').Gpio,
     led = new GPIO(18, 'out'),
-    button = new GPIO(17, 'in', 'both');
+    button = new GPIO(17, 'in', 'rising');
 
 // define the callback function
 function light(err, state) {
-
+  
+  if (err) console.log('err: ', err);
+  console.log('state: ', state);
   // check the state of the button
   // 1 == pressed, 0 == not pressed
   if(state == 1) {
     // turn LED on
     led.writeSync(1);
-  } else {
-    // turn LED off
+   setTimeout(function () {
+    console.log('LED off');
     led.writeSync(0);
+   }, 100)
   }
 
 }
