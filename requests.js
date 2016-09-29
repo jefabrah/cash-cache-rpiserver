4b7fe54b145da3ac6618fb6170a50cab81af976d
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-09-28 20:17:12 -0400

    coin value switch case

--- requests.js
+++ requests.js
@@ -2,6 +2,23 @@
 
  module.exports = {
    postCoinInput: function (pulseCount) {
-      console.log(pulseCount);
+      console.log('Pulse Count:',pulseCount);
+      // get coin value from pulse count
+      switch (pulseCount) {
+         case 1:
+            console.log('penny');
+            break;
+         case 2:
+            console.log('nickel');
+            break;
+         case 3:
+            console.log('dime');
+            break;
+         case 4:
+            console.log('quarter');
+            break;
+         default:
+            console.log('Invalid pulse');
+      }
    }
  };
