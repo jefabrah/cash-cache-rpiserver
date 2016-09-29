13c0d1e70afb15a22bf1f43c7e493a4d5fe5b8c8
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-09-28 20:27:40 -0400

    assign coin value in switch

--- requests.js
+++ requests.js
@@ -4,21 +4,37 @@
    postCoinInput: function (pulseCount) {
       console.log('Pulse Count:',pulseCount);
       // get coin value from pulse count
+      let coin;
       switch (pulseCount) {
          case 1:
             console.log('penny');
+            coin = 1;
             break;
          case 2:
             console.log('nickel');
+            coin = 5;
             break;
          case 3:
             console.log('dime');
+            coin = 10;
             break;
          case 4:
             console.log('quarter');
+            coin = 25;
             break;
          default:
             console.log('Invalid pulse');
+            return;
       }
+
+   //    axios.post('/api/coin', {
+   //     coin: coin
+   //   })
+   //   .then(function (response) {
+   //     console.log(response);
+   //   })
+   //   .catch(function (error) {
+   //     console.log(error);
+   //   });
    }
  };
